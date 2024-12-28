e1b3533c8f9d218898afe6306bcb9cb96b26f521
Jan-H-Christensen <janx802y@easv365.dk>
2024-12-28 01:59:40 +0000

    🐛 issue with start up docker on linux with rabbitMQ (fixed)

--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -6,8 +6,6 @@ using InventoryService.Model;
 using InventoryService.Service;
 using EasyNetQ;
 using EasyNetQ.DI.Microsoft;
-
-
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,16 +13,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
 builder.Services.AddSingleton<ProductService>();
 
-var bus = EasyNetQ.RabbitHutch.CreateBus(builder.Configuration.GetConnectionString("RabbitMQ"));
-builder.Services.AddSingleton<IBus>(bus);
-
+var rabbitMqConnectionString = builder.Configuration.GetConnectionString("RabbitMQ");
+if (!string.IsNullOrEmpty(rabbitMqConnectionString))
+{
+    var bus = RabbitHutch.CreateBus(rabbitMqConnectionString);
+    builder.Services.AddSingleton<IBus>(bus);
+}
+else
+{
+    builder.Services.AddSingleton<IBus, NullBus>();
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHostedService<ServiceRegistrarHostedService>();
 builder.Services.AddHostedService<InventoryMessageConsumer>();
-
 
 var app = builder.Build();
 
